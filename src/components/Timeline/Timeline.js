ee3e409584d894b603e44d1fb7758ccb00c92dc0
agherasie <alexandru.gherasie@epitech.eu>
2022-07-17 00:16:32 +0200

    [ADD] api call

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -15,9 +15,15 @@ export default function Timeline(props) {
         return day + "/" + month + "/" + year;
     }
 
+    const [allEvents, setAllEvents] = React.useState(events)
+
+    React.useEffect(() => {
+        fetch("https://localhost/events").then(res => res.json()).then(data => setAllEvents(data['hydra:member']))
+    }, [])
+
     var left = true;
 
-    const listEvents = events.sort((a, b) => new Date(a.date) - new Date(b.date)).filter(function(event) {
+    const listEvents = allEvents.sort((a, b) => new Date(a.date) - new Date(b.date)).filter(function(event) {
         if (new Date(event.date) < new Date(props.dateRange[0])
         || new Date(event.date) > new Date(props.dateRange[1])) {
             return false;
--- src/events.js
+++ src/events.js
@@ -4,28 +4,28 @@ export const events = [
         title: "Ezio Auditore da Firenze is born",
         date: "1459-06-24",
         filters: ["Births", "Ezio", "Auditore", "Assassins"],
-        url: "https://www.youtube.com/embed/_6MavSZjdLQ",
+        // url: "https://www.youtube.com/embed/_6MavSZjdLQ",
     },
     {
         id: 2,
         title: "Ezio Auditore da Firenze dies of a heart attack",
         date: "1524-11-30",
         filters: ["Deaths", "Ezio", "Auditore", "Assassins"],
-        url: "https://www.youtube.com/embed/hl05ubFXwrQ"
+        // url: "https://www.youtube.com/embed/hl05ubFXwrQ"
     },
     {
         id: 3,
         title: "Deaths of Giovanni, Federico and Petruccio Auditore",
         date: "1476-12-29",
         filters: ["Deaths", "Ezio", "Auditore", "Assassins"],
-        url: "https://www.youtube.com/embed/NgAjSiuBQco",
+        // url: "https://www.youtube.com/embed/NgAjSiuBQco",
     },
     {
         id: 4,
         title: "Ezio Auditore becomes Mentor of the Italian Brotherhood",
         date: "1503-08",
         filters: ["Ezio", "Auditore", "Assassins"],
-        url: "https://www.youtube.com/embed/7VVdcxhElq0",
+        // url: "https://www.youtube.com/embed/7VVdcxhElq0",
     },
     {
         id: 5,
@@ -38,13 +38,13 @@ export const events = [
         title: "Altair Ibn-La'Ahad dies",
         date: "1257-08-12",
         filters: ["Deaths", "Altair", "Assassins"],
-        url: "https://www.youtube.com/embed/jTxnjFRi2T0",
+        // url: "https://www.youtube.com/embed/jTxnjFRi2T0",
     },
     {
         id: 7,
         title: "Al Mualim is asssassinated by Altair",
         date: "1191-09",
         filters: ["Deaths", "Altair", "Al Mualim", "Assassins", "Templars"],
-        url: "https://www.youtube.com/embed/tvt-oAZ8JP8"
+        // url: "https://www.youtube.com/embed/tvt-oAZ8JP8"
     }
 ];
